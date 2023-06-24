ce102a638f0e4b583c3b7a73e3df0717d09dc9b4
Emir Avci <emiravc@gmail.com>
2023-06-24 16:49:52 -0400

    Logic update on voting 🚀

--- src/VoteOption.js
+++ src/VoteOption.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { ProgressBar } from 'react-bootstrap';
+import Button from '@mui/material/Button';
 import './VoteOption.css';
 
 const VoteOption = ({ country, handleVote }) => {
-  const [votes, setVotes] = useState({ yes: 0, no: 0 });
-  const [voteChangeCount, setVoteChangeCount] = useState(0);
+  const [votes, setVotes] = useState({ yes: Math.floor(Math.random() * 100) + 1, no: Math.floor(Math.random() * 100) + 1 });
   const [countryImage, setCountryImage] = useState(null);
-  const [lastVote, setLastVote] = useState(null);
+  const [selectedVote, setSelectedVote] = useState(null);
   const [warning, setWarning] = useState('');
 
   useEffect(() => {
-    const storedVote = localStorage.getItem(`vote-${country}`);
-    if (storedVote) {
-      setLastVote(storedVote);
-    }
-
     import(`./images/${country.toLowerCase()}.png`)
       .then((image) => {
         setCountryImage(image.default);
@@ -25,41 +20,58 @@ const VoteOption = ({ country, handleVote }) => {
   }, [country]);
 
   const vote = (type) => {
-    setVotes({ ...votes, [type]: votes[type] + 1 });
+    if (selectedVote === type) {
+      setSelectedVote(null);
+      setVotes(prevVotes => ({
+        ...prevVotes,
+        [type]: prevVotes[type] - 5,
+      }));
+    } else {
+      setSelectedVote(type);
+      setVotes(prevVotes => ({
+        ...prevVotes,
+        [type]: prevVotes[type] + 5,
+      }));
+    }
     handleVote(country, type);
-    setLastVote(type);
-    localStorage.setItem(`vote-${country}`, type);
   };
 
-  const getButtonColor = (type) => {
+  const getButtonVariant = (type) => {
     const totalVotes = votes.yes + votes.no;
-    if (totalVotes === 0) return '#333';
+    if (totalVotes === 0) return 'outlined';
     const percentage = votes[type] / totalVotes;
-    if (percentage > 0.5) return 'green';
-    if (percentage < 0.5) return 'red';
-    return 'navy';
+    if (percentage > 0.5) return 'contained';
+    if (percentage < 0.5) return 'outlined';
+    return 'outlined';
   };
 
   const totalVotes = votes.yes + votes.no;
   const yesPercentage = totalVotes ? (votes.yes / totalVotes) * 100 : 0;
   const noPercentage = totalVotes ? (votes.no / totalVotes) * 100 : 0;
 
+  
   return (
-    <div className="vote-option" style={ {paddingBottom: '20px'}}>
+    <div className="vote-option" style={{ paddingBottom: '20px' }}>
       {countryImage && <img src={countryImage} alt={country} />}
       <h2>{country}</h2>
-      <button 
-        style={{backgroundColor: getButtonColor('yes'), width: '150px', height: '50px', margin: '10px'}}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Button 
+        variant="contained"
         onClick={() => vote('yes')}
+        disabled={selectedVote === 'no'}
+        style={{width: '45%', backgroundColor: selectedVote === 'no' ? 'transparent' : '#333', color: 'white', marginBottom: '10px'}}
       >
-        Yes: ({((votes.yes / (votes.yes + votes.no || 1)) * 100).toFixed(2)}%)
-      </button>
-      <button 
-        style={{backgroundColor: getButtonColor('no'), width: '150px', height: '50px', margin: '10px'}}
+        Yes: {yesPercentage.toFixed(2)}%
+      </Button>
+      <Button 
+        variant="contained"
         onClick={() => vote('no')}
+        disabled={selectedVote === 'yes'}
+        style={{width: '45%', backgroundColor: selectedVote === 'yes' ? 'transparent' : '#333', color: 'white', marginBottom: '10px'}}
       >
-        No: ({((votes.no / (votes.yes + votes.no || 1)) * 100).toFixed(2)}%)
-      </button>
+        No: {noPercentage.toFixed(2)}%
+      </Button>
+      </div>
       <ProgressBar>
         <ProgressBar striped variant="success" now={yesPercentage} key={1} />
         <ProgressBar striped variant="danger" now={noPercentage} key={2} />
