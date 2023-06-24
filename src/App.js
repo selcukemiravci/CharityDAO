7fb68521b67e1d2ba39abb45ef6fd0803a89d6c6
Emir Avci <emiravc@gmail.com>
2023-06-24 00:42:57 -0400

    Updated Front end 🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const countries = ['USA', 'Canada', 'Mexico', 'France', 'Germany', 'Spain'];
+
+const VoteOption = ({ country, handleVote }) => {
+  const [displayButtons, setDisplayButtons] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="country-container" onClick={() => setDisplayButtons(!displayButtons)} style={{width: '370px', height: '260px'}}>
+      <h2>{country}</h2>
+      {displayButtons && (
+        <div>
+          <button onClick={(e) => {e.stopPropagation(); handleVote(country, 'yes')}}>Vote Yes</button>
+          <button onClick={(e) => {e.stopPropagation(); handleVote(country, 'no')}}>Vote No</button>
+          <button onClick={(e) => {e.stopPropagation(); handleVote(country, 'abstain')}}>Abstain</button>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+const App = () => {
+  const [votes, setVotes] = useState({});
+  const [prizePool, setPrizePool] = useState(0);
+
+  const handleVote = (country, vote) => {
+    setVotes({
+      ...votes,
+      [country]: vote,
+    });
+    setPrizePool(prizePool + 10); // increment prize pool by 10 for every vote
+  };
+
+  return (
+    <div className="App">
+      <h1>Country Voting</h1>
+      <div className="prize-pool">
+        <h2>Prize Pool: ${prizePool}</h2>
+      </div>
+      {countries.map((country) => (
+        <VoteOption key={country} country={country} handleVote={handleVote} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
