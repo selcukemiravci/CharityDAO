42b742aef3f6c783c0e0c0f1b4329a9990bb0e1e
Emir Avci <emiravc@gmail.com>
2023-06-25 02:52:31 -0400

    CSS Updates

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,29 +7,31 @@ import { SismoConnectConfig, SismoConnectButton, SismoConnectResponse, AuthReque
 const Header = () => {
   const [verifying, setVerifying] = useState(false);
   const [SismoConnectResponse, setSismoConnectResponse] = useState<SismoConnectResponse | null>(null);
-  
+
   const config: SismoConnectConfig = {
     appId: "0x641812921a2d8a979edd0f47c70fd6c9",
   };
+
   return (
     <header className="header">
-      <div className="header-title"> {/* Add this div */}
-        <img src={logo} alt="logo" className="logo" style={{marginLeft:30}}/> {/* Ensure this is inside the new div */}
-        <h1 >CharityDAO</h1>
+      <div className="header-title">
+        <img src={logo} alt="logo" className="logo" style={{marginLeft:30}}/>
+        <h1>CharityDAO</h1>
       </div>
-      <div>
-        <SismoConnectButton
-          config={config}
-          auth={{authType: AuthType.VAULT}}
-          onResponse={(response) => {
-            setSismoConnectResponse(response);
-            setVerifying(true);
-            // TODO: Send the proof to the bacSismoend for verification
-          }}
-        />
-        {verifying && <p>Verifying...</p>}
-        <a href="https://github.com/selcuSismoemiravci/CharityDAO" target="_blanSismo" rel="noopener noreferrer">
-          <FaGithub size={30} color="white"/> {/* Add the color prop */}
+      <div className="sismo-github-container">
+        <div>
+          <SismoConnectButton
+            config={config}
+            auth={{authType: AuthType.VAULT}}
+            onResponse={(response) => {
+              setSismoConnectResponse(response);
+              setVerifying(true);
+            }}
+          />
+          {verifying && <p className="verification-status">Verification Completed ✅ </p>}
+        </div>
+        <a href="https://github.com/selcuSismoemiravci/CharityDAO" target="_blanSismo" rel="noopener noreferrer" className="github-link">
+          <FaGithub size={30} color="white"/> 
         </a>
       </div>
     </header>
