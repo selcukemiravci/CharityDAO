b291b2da8316ebda2b1596db8d3829ba7fd0bbac
Emir Avci <emiravc@gmail.com>
2023-06-25 03:31:37 -0400

    Updated Version

--- src/sismo-connect-config.ts
+++
@@ -1,7 +0,0 @@
-// sismo-connect-config.ts
-
-import { SismoConnectConfig } from "@sismo-core/sismo-connect-react";
-
-export const config: SismoConnectConfig = {
-  appId: "0x641812921a2d8a979edd0f47c70fd6c9", 
-};
